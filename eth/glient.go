0856ca87c7cb5649da50ddba15213624bd4d6f42
Evan Kaminsky <evanskaminsky@gmail.com>
2018-04-20 14:21:42 -0500

    added rough draft of second pandian script

--- eth/glient.go
+++ eth/glient.go
@@ -24,6 +24,8 @@ func testA(eth *Ethereum) {
 
 	fmt.Printf("%v\n%v\n%v\n%v\n", ethAd, coinAd, pubAd, blockNum)
 
+	//am := accounts.NewManager(eth.ApiBackend
+
 	//fmt.Printf("~~~~~~~~~~~~~~~~~~~\n\n\n\n")
 	//fmt.Printf("%+v\n\n", backend.CurrentBlock())
 	//fmt.Printf("damn")
@@ -31,20 +33,15 @@ func testA(eth *Ethereum) {
 }
 
 
-func testB(eth *Ethereum) {
-	//am := accounts.NewManager(eth.ApiBackend)
-}
-
-
 func countAccounts(eth *Ethereum) {
-	blockNum := 0
-	trans := 0
-	blocks := 0
+	var blockNum = 0
+	var trans = 0
+	var blocks = 0
 	addresses := make(map[*common.Address]bool)
 
 	fmt.Printf("%s Starting to count accounts...\n", time.Now().String())
 
-	printEvery := 100000
+	var printEvery = 100000
 
 	for {
 		blck := blockNum
@@ -78,8 +75,6 @@ func countAccounts(eth *Ethereum) {
 
 	fmt.Printf("Total blocks processed: %v Total transactions: %v\n", blocks, trans)
 
-	//let positiveAddresses = []
-
 	var numAddresses = len(addresses)
 	fmt.Printf("%s Total number of accounts: %v\n", time.Now().String(), numAddresses)
 	var positiveAddresses = 0
@@ -88,7 +83,7 @@ func countAccounts(eth *Ethereum) {
 
 	for address := range addresses {
 		if address == nil {
-			fmt.Println(address)
+			fmt.Printf("??? Nil Address Found: %v\n", address)
 			continue
 		}
 
@@ -104,4 +99,107 @@ func countAccounts(eth *Ethereum) {
 	fmt.Printf("%s Exiting process.\n", time.Now().String())
 
 }
+
+
+func countAccountWithDBGetMetrics(eth *Ethereum) {
+	var blockNum = 0
+	var trans = 0
+	var blocks = 0
+	addresses := make(map[*common.Address]bool)
+
+	var gets = eth.MetricsDict()["user/gets"]
+	var getsNow = 0
+
+	var getBlockGets = 0
+	var getTransactionGets = 0
+	var getBalanceGets = 0
+
+	var zeroTransactionBlocks = 0
+
+	fmt.Printf("%s Starting to count accounts...\n", time.Now().String())
+
+	var printEvery = 100000
+
+	for {
+		blck := blockNum
+		blockNum += 1
+
+		block := eth.blockchain.GetBlockByNumber(uint64(blck))
+		if block == nil {
+			break
+		}
+
+		getsNow = eth.chainDb.MetricsDict()["user/gets"]
+		getBlockGets += getsNow - gets
+		gets = getsNow
+
+		trans = trans + block.Transactions().Len()
+		if block.Transactions().Len() == 0 {
+			zeroTransactionBlocks += 1
+		}
+
+		for i := 0; i < block.Transactions().Len(); i++ {
+			transaction := block.Transactions()[i]
+
+			getsNow = eth.chainDb.MetricsDict()["user/gets"]
+			getTransactionGets += getsNow - gets
+			gets = getsNow
+
+			if transaction.Value().Sign() > 0 {
+				addresses[transaction.To()] = true
+			}
+		}
+
+		blocks += 1
+
+		if blocks % printEvery == 0 {
+			fmt.Printf("%s %v blocks processed. Number of transactions til block number %v: %v transactions. Number of accounts: %v\n",
+				time.Now().String(), blocks, blck, trans, len(addresses))
+			fmt.Printf("%s Get metrics - getBlockGets: %v, getTransactionGets: %v\n",
+				time.Now().String(), getBlockGets, getTransactionGets)
+		}
+	}
+
+	fmt.Printf("%s Total blocks processed: %v Total transactions: %v, Number of blocks with zero transactions: %v\n",
+		time.Now().String(), blocks, trans, zeroTransactionBlocks)
+
+	var numAddresses = len(addresses)
+	fmt.Printf("%s Total number of accounts: %v\n", time.Now().String(), numAddresses)
+	var positiveAddresses = 0
+
+	publicBlockChainAPI := ethapi.NewPublicBlockChainAPI(eth.ApiBackend)
+
+	for address := range addresses {
+		if address == nil {
+			fmt.Printf("??? Nil Address Found: %v\n", address)
+			continue
+		}
+
+		balance, err := publicBlockChainAPI.GetBalance(nil, *address, rpc.LatestBlockNumber)
+		if err != nil {
+			fmt.Printf("%s Error in GetBalance(): %v\n", time.Now().String(), err)
+			continue
+		}
+
+		getsNow = eth.chainDb.MetricsDict()["user/gets"]
+		getBalanceGets += getsNow - gets
+		gets = getsNow
+
+		if balance.Sign() > 0 {
+			positiveAddresses += 1
+		}
+	}
+
+	fmt.Printf("%s Get metrics - getBlockGets: %v, getTransactionGets: %v, getBalanceGets: %v\n",
+		time.Now().String(), getBlockGets, getTransactionGets, getBalanceGets)
+	fmt.Printf("%s Number of accounts with positive balance: %v \n", time.Now().String(), positiveAddresses)
+	fmt.Printf("%s Exiting process.\n", time.Now().String())
+
+}
+
+
+
+
+
+
 
